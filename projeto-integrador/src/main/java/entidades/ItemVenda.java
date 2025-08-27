e7782bbd13247395c7ea3e3986f24156a2cb8db6
Bruna Andrade Alves <179890@unifio.edu.br>
2025-08-27 23:36:28 +0000

    Aula dia 27/08/2025

---
+++ projeto-integrador/src/main/java/entidades/ItemVenda.java
@@ -0,0 +1,21 @@
+package entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class ItemVenda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer codigo;
+
+    public Double valorParcial;
+
+    public Double quantidadeParcial;
+
+    @ManyToOne
+    public Produto produto;
+}
